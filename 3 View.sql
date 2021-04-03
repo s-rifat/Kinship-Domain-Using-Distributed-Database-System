041645ad1dd7b1d95a210d6f2bf975f2da2121ff
Syed Rifat <gtr06.rft@gmail.com>
2021-04-03 20:13:42 +0600

    Update 3 View.sql

--- 3 View.sql
+++ 3 View.sql
@@ -2,3 +2,6 @@ clear screen;
 SELECT * FROM Parent1 NATURAL JOIN Parent2@site1;
 select * from Female1 union select * from Female2@site1;
 select * from Male1 union select * from Male2@site1;
+
+select * from (select * from Female1 union select * from Female2@site1) where name = 'Jerin';
+SELECT * FROM Parent1 NATURAL JOIN Parent2@site1 where id = 5;
